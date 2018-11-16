70040819751580f00efafba9410c28becedc8102
Emily Kauffman <emily.kauffman@discovery-robotics.com>
2018-11-16 09:49:13 -0500

    debugging

--- server/app.js
+++ server/app.js
@@ -66,7 +66,7 @@ if (process.env.NODE_ENV === 'production') {
     //     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     // });
     app.get('/', function (req, res) {
-        res.send('hello');
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
 
