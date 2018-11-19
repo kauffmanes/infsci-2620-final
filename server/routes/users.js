08ffef28992cf99896cf4247ef12441fa4045fc7
Lazarus1993 <ashutoshburde@gmail.com>
2018-11-18 21:49:17 -0500

    Frontend basic setup

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,19 +1,20 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const User = require('../models/User');
-const AccessLevel = require('../models/AccessLevel');
+const express = require("express");
+const User = require("../models/User");
+const AccessLevel = require("../models/AccessLevel");
 const usersRouter = express.Router();
-const Token = require('../utils/token');
+const Token = require("../utils/token");
 
-// endpoint: /api/users/
-usersRouter.route('/')
+// endpoint: /api/users/register
+usersRouter
+  .route("/register")
 
   // register new user
   .post(async (req, res) => {
-
+    console.log(req);
     const user = new User();
-    
+
     user.firstName = req.body.firstName;
     user.lastName = req.body.lastName;
     user.email = req.body.email;
@@ -22,16 +23,22 @@ usersRouter.route('/')
     user.title = req.body.titleId;
     user.displayName = req.body.displayName;
     user.accessLevel = req.body.accessLevel;
-
     // do error handling on what's required
-    if (!user.firstName || !user.lastName || !user.email || !user.password || !user.employer || !user.title || !user.displayName) {
-      return res.status(400).send('All fields are required.');
+    if (
+      !user.firstName ||
+      !user.lastName ||
+      !user.email ||
+      !user.password ||
+      !user.employer ||
+      //!user.title ||
+      !user.displayName
+    ) {
+      return res.status(400).send("All fields are required.");
     }
 
     // if none provided, default to regular user
 
     try {
-
       const accessObj = await AccessLevel.findOne({
         level: req.body.accessLevel || 1
       });
@@ -39,31 +46,36 @@ usersRouter.route('/')
       user.accessLevel = accessObj;
 
       // save user
-      user.save((err) => {
-
-        if (err && err.name === 'MongoError' && err.code === 11000) {
+      user.save(err => {
+        if (err && err.name === "MongoError" && err.code === 11000) {
           return res.status(400).send({
             status: 400,
-            statusText: 'A user with that email or display name already exists.'
+            statusText: "A user with that email or display name already exists."
           });
         }
 
         if (err) {
           console.log(err);
-          return res.status(500).send({ status: 500, statusText: 'Unable to save user.' });
+          return res
+            .status(500)
+            .send({ status: 500, statusText: "Unable to save user." });
         }
-        
+
         return User.findById(user._id, (err, user) => {
-          if (err) { console.log(err); return res.status(500).send({ status: 500, statusText: 'Could not find newly created user.' }); }
+          if (err) {
+            console.log(err);
+            return res.status(500).send({
+              status: 500,
+              statusText: "Could not find newly created user."
+            });
+          }
           return res.status(201).send(user);
-        })
+        });
       });
-
-    } catch(err) {
+    } catch (err) {
       console.log(err);
-      return res.status(500).send('Could not find that access level.');
+      return res.status(500).send("Could not find that access level.");
     }
-
   });
 
 /*
@@ -73,17 +85,15 @@ usersRouter.route('/')
   update account info
   get account by ID
   */
-usersRouter.post('/authenticate', async (req, res) => {
-
+usersRouter.post("/authenticate", async (req, res) => {
   if (!req.body.email || !req.body.password) {
     return res.status(400).send({
       status: 400,
-      statusText: 'An email and password are required'
+      statusText: "An email and password are required"
     });
   }
 
   try {
-
     const user = await User.findOne(
       { email: req.body.email }, // query
       { password: 1 } // projection
@@ -92,7 +102,7 @@ usersRouter.post('/authenticate', async (req, res) => {
     if (!user) {
       return res.status(404).send({
         status: 404,
-        statusText: 'No user was found for that email.',
+        statusText: "No user was found for that email.",
         success: false
       });
     }
@@ -102,39 +112,36 @@ usersRouter.post('/authenticate', async (req, res) => {
     if (!isValidPassword) {
       return res.status(400).send({
         status: 400,
-        statusText: 'Invalid email/password combination.',
+        statusText: "Invalid email/password combination.",
         success: false
       });
     }
 
     // set user's permission level (scope)
     let scope;
-    if (user.accessLevel === 2) {  scope = 'admin'; }
-    else if (user.accessLevel === 3) { scope = 'developer'; }
-    else { scope = 'user'; }
+    if (user.accessLevel === 2) {
+      scope = "admin";
+    } else if (user.accessLevel === 3) {
+      scope = "developer";
+    } else {
+      scope = "user";
+    }
 
-    const token = Token.sign(
-      { id: user._id, scope },
-      {  expiresIn: 86400 }
-    );  // expires in 24 hours
+    const token = Token.sign({ id: user._id, scope }, { expiresIn: 86400 }); // expires in 24 hours
 
     return res.status(200).send({
       success: true,
       token
     });
-
-  } catch(err) {
-
+  } catch (err) {
     console.log(err);
 
     return res.status(500).send({
       success: false,
-      statusText: 'Unable to authenticate.',
+      statusText: "Unable to authenticate.",
       status: 500
     });
-
   }
-
 });
 
 module.exports = usersRouter;
